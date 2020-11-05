1d6120da514e85d4c2247a70eb1566735965e2da
Jimmi <39318274+jamesyen16@users.noreply.github.com>
2020-11-04 20:58:59 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
-text = " Dio !!!!
+text = """ Dio !!!!
 Hoho, you are approaching me
 I cannot beat the sh*t out of you without getting closer
-Then get as close as you want. ZAAAA WORRRRDO! "
+Then get as close as you want. ZAAAA WORRRRDO! """
 
 print(text)
